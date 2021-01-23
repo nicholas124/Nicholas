3a1d301261843686ee12a56a6df82af9d839f2a2
nicholas124 <nmuchelemba@gmail.com>
2021-01-23 21:42:39 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -76,15 +76,3 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
-
-const imgContent = document.querySelectorAll('.img-content-hover');
-
-function showImgContent(e) {
-  for(var i = 0; i < imgContent.length; i++) {
-    x = e.pageX;
-    y = e.pageY;
-    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
-  }
-};
-
-document.addEventListener('mousemove', showImgContent);
